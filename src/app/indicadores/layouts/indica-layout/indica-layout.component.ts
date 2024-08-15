0933275eb43c525d8599497151f6c73115416751
Ildefonso Valenzuela <ponchovalo@outlook.com>
2024-08-15 16:44:06 -0600

    sidemenu

--- src/app/indicadores/layouts/indica-layout/indica-layout.component.ts
+++ src/app/indicadores/layouts/indica-layout/indica-layout.component.ts
@@ -2,8 +2,7 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-indica-layout',
-  templateUrl: './indica-layout.component.html',
-  styleUrl: './indica-layout.component.css'
+  templateUrl: './indica-layout.component.html'
 })
 export class IndicaLayoutComponent {
 
--- src/app/shared/components/side-menu/side-menu.component.ts
+++ src/app/shared/components/side-menu/side-menu.component.ts
@@ -3,7 +3,7 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'shared-side-menu',
   templateUrl: './side-menu.component.html',
-  styles: ``
+  styleUrls: ['./side-menu.component.css']
 })
 export class SideMenuComponent {
 
