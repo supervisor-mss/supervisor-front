98b8bd3cc9eedfbf6018f66afc53ddffd4c0e5fd
Ildefonso Valenzuela <ponchovalo@outlook.com>
2024-08-18 09:37:38 -0600

    mod

--- src/app/indicadores/layouts/indica-layout/indica-layout.component.ts
+++ src/app/indicadores/layouts/indica-layout/indica-layout.component.ts
@@ -2,7 +2,8 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-indica-layout',
-  templateUrl: './indica-layout.component.html'
+  templateUrl: './indica-layout.component.html',
+  styleUrls: ['./indica-layout.component.css']
 })
 export class IndicaLayoutComponent {
 
