869d058ce1e25827c08b0bc82842ca1922b4ad1f
Ildefonso Valenzuela <ponchovalo@outlook.com>
2024-08-18 22:51:29 -0600

    nva-act

--- src/app/indicadores/pages/nueva-actividad/nueva-actividad.component.ts
+++ src/app/indicadores/pages/nueva-actividad/nueva-actividad.component.ts
@@ -7,4 +7,34 @@ import { Component } from '@angular/core';
 })
 export class NuevaActividadComponent {
 
+  tipeActivitySelected: string = "NO_PLANIFICADO"
+
+  active1: string = ''
+  active2: string = ''
+  active3: string = ''
+
+
+  changeStatus(typeActivity: string){
+    this.tipeActivitySelected = typeActivity;
+    switch(this.tipeActivitySelected){
+      case 'NO_PLANIFICADO':
+        this.active1 = 'is-active';
+        this.active2 = '';
+        this.active3 = '';
+        break;
+      case 'PREVENTIVO':
+        this.active1 = '';
+        this.active2 = 'is-active';
+        this.active3 = '';
+        break;
+      case 'ACT_ADM':
+        this.active1 = '';
+        this.active2 = '';
+        this.active3 = 'is-active';
+        break;
+      default:
+        return;
+    }
+  }
+
 }
